bdd3c967e4bd4a43a11ce51145202a9ef251c645
Bohdan Purtell <bohdanpurtell@gmail.com>
2026-05-04 04:12:31 -0400

    moving stuff

--- mii/mii_of_hardcaml/src/rx_controller_intf.ml
+++
@@ -1,7 +0,0 @@
-open! Base
-(* open! Hardcaml *)
-(* open! Signal *)
-
-let () = 
-  Stdio.print_endline "=== Imported RX_CONTROLLER_INTF ==="
-
--- rgmii/src/eth_mac.ml
+++
@@ -1,5 +0,0 @@
-
-
-
-let () =
-  print_endline "eth mac library imported!!!"
