aaa65f44fafba447e96d84eedd6055f1502a1b5c
wayne <bohdanpurtell@gmail.com>
2026-05-06 11:23:58 -0700

    running out a UART module; few different things to do; will figure out later

--- lib/common/generate.ml
+++ lib/common/generate.ml
@@ -1,16 +1,20 @@
 open! Core
 open! Hardcaml
 open! Mii_of_hardcaml
+open! Uart_of_hardcaml
 open! Signal
 
-module Circ = Circuit.With_interface (Mac_top.I) (Mac_top.O)
+(* module Circ = Circuit.With_interface (Mac_top.I) (Mac_top.O) *)
+module Circ = Circuit.With_interface (Uart_top.I) (Uart_top.O)
 
 let () =
   Stdio.print_endline "============ Begin Generation Phase =============== ";
 
   let scope = Scope.create ~flatten_design:false () in
-  let circ = Circ.create_exn ~name:"mac_top" (Mac_top.create scope) in
+  (* let circ = Circ.create_exn ~name:"mac_top" (Mac_top.create scope) in *)
+  let circ = Circ.create_exn ~name:"uart_tx" (Uart_top.create scope) in
   let hier = Rtl.create Verilog [circ] in
   let rtl  = Rtl.full_hierarchy hier in
-  Out_channel.write_all "mac_top.v" ~data:(Rope.to_string rtl)
+  (* Out_channel.write_all "mac_top.v" ~data:(Rope.to_string rtl) *)
+  Out_channel.write_all "uart_tx.v" ~data:(Rope.to_string rtl)
 
---
+++ lib/uart/uart_top.ml
@@ -0,0 +1,145 @@
+open! Core
+open! Hardcaml
+open! Signal
+open! Always
+
+let () =
+  Stdio.print_endline "=== Imported UART TX Top ===";
+
+module I = struct
+  type 'a t = {
+    clk : 'a;
+    rst : 'a;
+    en  : 'a;
+
+    tick : 'a;
+
+    d_in : 'a [@bits 8];
+    d_in_valid : 'a;
+  } [@@deriving hardcaml]
+end
+
+module O = struct
+  type 'a t = {
+    uart_tx : 'a;
+
+    keep : 'a;
+  } [@@deriving hardcaml]
+end
+
+module States = struct
+  type t =
+    | IDLE (* idle high *)
+    | START
+    | PAYLOAD
+    | STOP
+    | DONE
+    [@@deriving sexp_of, compare ~localize, enumerate]
+end
+
+let create 
+  (scope : Scope.t)
+  (i)
+  : (_ O.t)
+  =
+
+  (* scope shenanigans *)
+  let _scope = Scope.sub_scope scope "uart_tx" in
+
+  (* port aliases *)
+  let clk = i.I.clk in
+  let rst = i.I.rst in
+  let en  = i.I.en in
+  let byte = i.I.d_in in
+  let byte_valid = i.I.d_in_valid in
+  let rising_edge : Reg_spec.t = Reg_spec.create ~clock:clk ~clear:rst () in
+  let tx_d = Always.Variable.wire ~default:(Signal.one 1) () in
+
+  (* state machine *)
+  let sm = Always.State_machine.create (module States) ~enable:en rising_edge in
+
+  (* internals *)
+  let data_place_counter = Always.Variable.reg ~enable:en ~width:3 rising_edge in
+
+  let frame =
+    concat_msb [
+      Signal.zero 1;
+      byte;
+      Signal.one 1;
+    ] in
+
+  Always.(
+    (* moore assignment *)
+    compile [
+      (* default *)
+      tx_d <--. 1;
+
+      sm.switch ~default:[] [
+        IDLE, [
+          tx_d <--. 1;
+        ];
+
+        START, [
+          tx_d <--. 0;
+        ];
+
+        PAYLOAD, [
+          (* bitmasked shift register, is there a more OCaml way for this? *)
+          (* let target = (srl byte ~by:1) in *)
+          (* tx_d <-- target; *)
+          (* let shifted = srl byte data_place_counter.value in *)
+          (* () *)
+          let tx_bit = mux data_place_counter.value (bits_lsb byte) in
+          tx_d <-- tx_bit;
+        ];
+
+        STOP, [
+          tx_d <--. 1;
+        ];
+      ];
+    ];
+
+    (* mealy next_state *)
+    compile [
+      sm.switch ~default:[] [
+        IDLE, [
+          if_ (byte_valid) [
+            sm.set_next START;
+          ] [
+            sm.set_next IDLE;
+          ];
+        ];
+
+        START, [
+          sm.set_next PAYLOAD;
+        ];
+
+        PAYLOAD, [
+          (* we can do this with a hardware Always counter, but is there a more ocaml-y way with ocaml? *)
+          (* there is indeed with the reg_fb primitive *)
+          sm.set_next PAYLOAD;
+
+          when_ (i.I.tick) [
+            if_ (data_place_counter.value ==: of_int_trunc ~width:3 7) [
+              (* move to stop condition, line should be driven to mark *)
+              sm.set_next STOP;
+            ] [
+              (* keep payloading *)
+              data_place_counter <-- data_place_counter.value +:. 1;
+              sm.set_next PAYLOAD;
+            ];
+          ]
+        ];
+
+        STOP, [
+          sm.set_next IDLE;
+        ];
+      ];
+    ];
+  );
+
+  {
+    uart_tx = tx_d.value;
+    keep    = zero 1;
+  }
+
