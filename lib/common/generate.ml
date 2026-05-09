a839eafe8820d0f806174926079a82c0fb84f19d
wayne <bohdanpurtell@gmail.com>
2026-05-08 17:50:58 -0700

    turns out i was driving the wrong pin for the TX; (need to drive the D10 pin -> uart_rx named which is weird but whatever); UART module needs some nicer integration now perhaps

--- lib/common/generate.ml
+++ lib/common/generate.ml
@@ -5,14 +5,14 @@ open! Uart_of_hardcaml
 open! Signal
 
 (* module Circ = Circuit.With_interface (Mac_top.I) (Mac_top.O) *)
-module Circ = Circuit.With_interface (Uart_top.I) (Uart_top.O)
+module Circ = Circuit.With_interface (Uart_tx.I) (Uart_tx.O)
 
 let () =
   Stdio.print_endline "============ Begin Generation Phase =============== ";
 
   let scope = Scope.create ~flatten_design:false () in
   (* let circ = Circ.create_exn ~name:"mac_top" (Mac_top.create scope) in *)
-  let circ = Circ.create_exn ~name:"uart_tx" (Uart_top.create scope) in
+  let circ = Circ.create_exn ~name:"uart_tx" (Uart_tx.create scope) in
   let hier = Rtl.create Verilog [circ] in
   let rtl  = Rtl.full_hierarchy hier in
   (* Out_channel.write_all "mac_top.v" ~data:(Rope.to_string rtl) *)
